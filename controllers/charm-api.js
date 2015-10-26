73a2b2d9533a2ade798717db026dc237f17587a2
Thuc Tran <ttran92@gatech.edu>
2015-10-26 15:38:32 -0400

    update some params

--- controllers/charm-api.js
+++ controllers/charm-api.js
@@ -3,19 +3,18 @@ var fhir = require('fhir-node');
 module.exports = function(req, res){
 
 	var params = {
-		pages: 'd33d98b9-91b8-49b0-b6f3-256e334f5437',
 		start: 0,
-		end: 500,
+		end: 250,
 		format: 'json'
 	};
 	
 	// for a promise
-	fhir(params).then(function(records){
+	fhir().then(function(records){
 		res.json(records);
 	});
 	
 	// for a callback
-	// fhir(params, function(records){
-	// 	res.json(records);
-	// });
+	fhir(params, function(records){
+		res.json(records);
+	});
 };
